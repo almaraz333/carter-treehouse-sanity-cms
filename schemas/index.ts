c1cdce31e8c8ead76e6b3c0029d232d6b3384357
Colton Almaraz <almarazcolton333@gmail.com>
2023-09-23 15:00:18 -0700

    Feature: add mar markers

--- schemas/index.ts
+++ schemas/index.ts
@@ -2,5 +2,6 @@ import image from './sectionImage'
 import textBlock from './block'
 import sectionTitle from './sectionTitle'
 import offering from './offering'
+import mapMarker from './mapMarker'
 
-export const schemaTypes = [image, textBlock, sectionTitle, offering]
+export const schemaTypes = [image, textBlock, sectionTitle, offering, mapMarker]
---
+++ schemas/mapMarker.ts
@@ -0,0 +1,27 @@
+export default {
+  title: 'Map Marker',
+  name: 'map marker',
+  type: 'document',
+  fields: [
+    {
+      title: 'Latitude',
+      name: 'latitude',
+      type: 'number',
+    },
+    {
+      name: 'longitude',
+      type: 'number',
+      title: 'Longitude',
+    },
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name',
+    },
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description',
+    },
+  ],
+}
